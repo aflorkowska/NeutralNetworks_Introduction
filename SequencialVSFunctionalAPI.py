f946308119a67e3199b16249917a6087bc503b0c
Agnieszka Florkowska <aga.florkowska99@gmail.com>
2024-01-07 20:09:07 +0100

    Sequencial vs Functional API - comparison

--- SequencialVSFunctionalAPI.py
+++ SequencialVSFunctionalAPI.py
@@ -3,47 +3,39 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 # Sequential vs Functional API
-import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.datasets import mnist
-
-# Using GPU
-physical_devices = tf.config.list_physical_devices("GPU")
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(-1, 28 * 28).astype("float32") / 255.0
-x_test = x_test.reshape(-1, 28 * 28).astype("float32") / 255.0
+from tensorflow.keras.utils import plot_model
 
 # Sequential API (Very convenient, not very flexible)
-#model = keras.Sequential(
-#    [
-#        keras.Input(shape=(28 * 28)),
-#        layers.Dense(512, activation="relu"),
-#        layers.Dense(256, activation="relu"),
-#        layers.Dense(10),
-#    ]
-#)
-
-#model = keras.Sequential()
-#model.add(keras.Input(shape=(784)))
-#model.add(layers.Dense(512, activation="relu"))
-#model.add(layers.Dense(256, activation="relu", name="my_layer"))
-#model.add(layers.Dense(10))
-
-# Functional API (A bit more flexible)
-inputs = keras.Input(shape=(784))
-x = layers.Dense(512, activation="relu", name="first_layer")(inputs)
-x = layers.Dense(256, activation="relu", name="second_layer")(x)
-outputs = layers.Dense(10, activation="softmax")(x)
-model = keras.Model(inputs=inputs, outputs=outputs)
-
-model.compile(
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-    optimizer=keras.optimizers.Adam(lr=0.001),
-    metrics=["accuracy"],
+modelA = keras.Sequential(
+   [
+       keras.Input(shape=(28 * 28)),
+       layers.Dense(512, activation="relu"),
+       layers.Dense(256, activation="relu"),
+       layers.Dense(10),
+   ]
 )
 
-model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
-model.evaluate(x_test, y_test, batch_size=32, verbose=2)
+modelB = keras.Sequential()
+modelB.add(keras.Input(shape=(784)))
+modelB.add(layers.Dense(512, activation="relu"))
+modelB.add(layers.Dense(256, activation="relu", name="my_layer"))
+modelB.add(layers.Dense(10))
+
+# Functional API (A bit more flexible, allowing for more complex models with multiple inputs or outputs, shared layers, and non-linear flows)
+inputs = keras.Input(shape=(784))
+xA = layers.Dense(512, activation="relu", name="first_layer")(inputs)
+xA = layers.Dense(256, activation="relu", name="second_layer")(xA)
+xB = layers.Dense(128, activation="relu", name="third_layer")(xA)
+outputsA = layers.Dense(10, activation="softmax")(xA)
+outputsB = layers.Dense(10, activation="softmax")(xB)
+modelC = keras.Model(inputs=inputs, outputs=[outputsA, outputsB])
+
+print(modelB.summary())
+plot_model(modelB, to_file='Sequential.png', show_shapes=False, show_dtype=False,
+show_layer_names=True, rankdir='TB', expand_nested=False, dpi=300)
+
+print(modelC.summary())
+plot_model(modelC, to_file='Functional.png', show_shapes=False, show_dtype=False,
+show_layer_names=True, rankdir='TB', expand_nested=False, dpi=300)
