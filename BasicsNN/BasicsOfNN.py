941a92be8c3d12ad0d71fe02d21794b84d11dbd3
Agnieszka Florkowska <aga.florkowska99@gmail.com>
2024-01-09 01:55:18 +0100

    Added creating model, traning, evaluaton and testing parts.

--- BasicsNN/BasicsOfNN.py
+++ BasicsNN/BasicsOfNN.py
@@ -7,6 +7,9 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from keras import backend as K
 
 import plotMethods
 
@@ -14,28 +17,126 @@ import plotMethods
 physical_devices = tf.config.list_physical_devices("GPU")
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-# Data loading and statistics
+# Data loading, splitting and statistics
+print(f"---LOADING---")
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, random_state=0, train_size = .5)
 print(f"Training data size: {x_train.shape[0]}")
+print(f"Validation data size: {x_val.shape[0]}")
 print(f"Testing data size: {x_test.shape[0]}")
 print(f"Single input data dimension (= image size): {x_test.shape[1]} x {x_test.shape[2]}")
 num_classes = np.size(np.unique(y_train))
-print(f"Number of classes {num_classes}")
+labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+print(f"Number of classes {num_classes} with labels {labels}")
 
-trainDistribution = plotMethods.bar(np.unique(y_train, return_counts=True)[0], np.unique(y_train, return_counts=True)[1], "Distribution of the numbers training data of various classes")
-trainDistribution.show()
-testDistribution = plotMethods.bar(np.unique(y_test, return_counts=True)[0], np.unique(y_test, return_counts=True)[1], "Distribution of the numbers testing data of various classes")
-testDistribution.show()
+#trainDistribution = plotMethods.bar(np.unique(y_train, return_counts=True)[0], np.unique(y_train, return_counts=True)[1], "Distribution of the numbers training data of various classes")
+#trainDistribution.show()
+#valDistribution = plotMethods.bar(np.unique(y_val, return_counts=True)[0], np.unique(y_test, return_counts=True)[1], "Distribution of the numbers validation data of various classes")
+#valDistribution.show()
+#testDistribution = plotMethods.bar(np.unique(y_test, return_counts=True)[0], np.unique(y_test, return_counts=True)[1], "Distribution of the numbers testing data of various classes")
+#testDistribution.show()
 
-trainSetSamples = plotMethods.samplesOfSet(x_train, y_train, "Training set - examples", 2, 5, 0, 10)
-trainSetSamples.show()
-testSetSamples = plotMethods.samplesOfSet(x_test, y_test, "Testing set - examples", 2, 5, 0, 10)
-testSetSamples.show()
+#trainSetSamples = plotMethods.samplesOfSet(x_train, y_train, "Training set - examples", 2, 5, 0, 10)
+#trainSetSamples.show()
+#valSetSamples = plotMethods.samplesOfSet(x_val, y_val, "Validation set - examples", 2, 5, 0, 10)
+#valSetSamples.show()
+#testSetSamples = plotMethods.samplesOfSet(x_test, y_test, "Testing set - examples", 2, 5, 0, 10)
+#testSetSamples.show()
+print(f"")
 
 # Preprocessing
+print(f"---PREPROCESSING---")
+print(f"BEFORE preprocessing: training data size: {x_train.shape}, validation data size: {x_val.shape} and testing data size: {x_test.shape}")
+x_train = x_train.reshape(-1, x_train.shape[1] * x_train.shape[2]).astype("float32") / 255.0
+x_val = x_val.reshape(-1, x_val.shape[1] * x_val.shape[2]).astype("float32") / 255.0
+x_test = x_test.reshape(-1, x_test.shape[1] * x_test.shape[2]).astype("float32") / 255.0
+print(f"AFTER preprocessing: training data size: {x_train.shape}, validation data size: {x_val.shape} and testing data size: {x_test.shape}")
+
+#y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+#y_val = tf.keras.utils.to_categorical(y_val, num_classes)
+#y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+print(f"")
 
 # Model
+print(f"---MODEL---")
+inputs = keras.Input(shape=(x_train.shape[1]))
+x = layers.Dense(512, activation="relu", name="first_layer")(inputs)
+x = layers.Dense(256, activation="relu", name="second_layer")(x)
+outputs = layers.Dense(10, activation="softmax")(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
+    metrics=["acc"],
+)
+print(model.summary())
+print(f"")
 
 # Training
+print(f"---TRAINING---")
+history = model.fit(x_train, y_train, batch_size=32, epochs=3, verbose=2, validation_data=(x_val, y_val))
+print(f"")
 
 # Evaluation
+print(f"---EVALUATION---")
+train_loss, train_acc = model.evaluate(x_train, y_train, batch_size=32, verbose=2)
+print(f"On training data model obtained accuracy equals to {train_acc:.2f} wit loss {train_loss:.2f}. It means that bias is {train_acc:.2f}, assuming that the Bayes optimal error is equal to 0.")
+
+val_loss, val_acc = model.evaluate(x_val, y_val, batch_size=32, verbose=2)
+print(f"On validation data model obtained accuracy equals to {val_acc:.2f} wit loss {val_loss:.2f}. It means that variance is {(val_acc - train_acc):.2f},")
+
+#accuracyComparison = plotMethods.compareResults(history.history['acc'], history.history['val_acc'], "Model accuracy", "Epoch", "Accuracy")
+#accuracyComparison.show()
+#lossComparison = plotMethods.compareResults(history.history['loss'], history.history['val_loss'], "Model loss", "Epoch", "Loss")
+#lossComparison.show()
+
+argmax = np.argmax(history.history['val_acc'])
+acc_max = history.history['val_acc'][argmax]
+print(f'The best accuracy {acc_max:.2f} for validation set we achieved in epoch: {argmax:.2f}')
+
+argmin = np.argmin(history.history['val_loss'])
+loss_min = history.history['val_loss'][argmin]
+print(f'Overfitting stated after epoch:  {argmin:.2f} where the minimum loss was: {loss_min:.2f}')
+
+print(f"")
+
+#Testing
+print(f"---TESTING---")
+y_test_prediction = model.predict(x_test)
+y_test_predictionMAX = np.argmax(y_test_prediction, axis = 1) # taking the class with the highest probability
+confusionMatrix = plotMethods.show_confusion_matrix(y_test, y_test_predictionMAX, num_classes, labels)
+#confusionMatrix.show()
+
+print(classification_report(y_test, y_test_predictionMAX))
+print(f"")
+
+# # Finding out misclassified examples
+classcheck = y_test - y_test_predictionMAX # 0 when the class is the same, otherwise example is misclassified
+misclasified = np.where(classcheck != 0)[0]
+num_misclassified = len(misclasified)
+# Verify if it works correctly
+# Refractor method
+misclassifiedSetSamples = plotMethods.misclassificationReport(x_test, misclasified, y_test, y_test_predictionMAX, "Misclassification report in format [true label : prediction]", 2, 5, 0, num_misclassified)
+misclassifiedSetSamples.show()
+
+
+###### TODO
+### Basic part
+# Labels encoding
+# ROC curves
+# DataLoaders
+# Data augmentation
+# Regularizations
+# Types of layers
+# Dropout
+# Callbacks
+# Tensorflow board
+# Saving models and loading them
+
+### Advanced, useful and detailed
+# Custom training loops
+# Custom dataset
+# Custom layers
+# Custom model.fit method
+
--- BasicsNN/plotMethods.py
+++ BasicsNN/plotMethods.py
@@ -1,5 +1,8 @@
 
 import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+from sklearn.metrics import confusion_matrix
 
 def bar(x: any,
         y: any,
@@ -19,16 +22,87 @@ def samplesOfSet(data: any,
                  ncols: int,
                  firstIndex: int,
                  lastIndex: int,
+                 height: int = 28,
+                 width: int = 28,
                  cmap: str = "gray"):
+    ncols = math.ceil(len(data) / ncols)
     fig = plt.figure(figsize=(10, 5))
+    chooseFirstIndex = lambda a, b: 0 if (a > b) else a
+    chooseLastIndex = lambda a, b : a if(a < b) else b
+    firstIndex = chooseFirstIndex(firstIndex, len(data))
+    lastIndex = chooseLastIndex(len(data), lastIndex)
     for index in range(firstIndex, lastIndex):
         plt.subplot(nrows, ncols, index + 1)
-        plt.imshow(data[index], cmap=cmap)
+        plt.imshow(data[index].reshape(height, width), cmap=cmap)
         plt.title("Class " + str(label[index]))
         plt.axis('off')
     plt.ioff()
     fig.suptitle(title)
     return plt
 
+def misclassificationReport(data: any,
+                            indexesMisclassified: any,
+                            trueLabel: any,
+                            predictedLabel: any,
+                            title: str,
+                            nrows: int,
+                            ncols: int,
+                            firstIndex: int,
+                            lastIndex: int,
+                            height: int = 28,
+                            width: int = 28,
+                            cmap: str = "gray") :
+    chooseFirstIndex = lambda a, b: 0 if (a > b) else a
+    chooseLastIndex = lambda a, b: a if (a < b) else b
+    firstIndex = chooseFirstIndex(firstIndex, len(indexesMisclassified))
+    lastIndex = chooseLastIndex(ncols, lastIndex)
+    fig = plt.figure(figsize=(10, 5))
+    for index in range(firstIndex, lastIndex):
+        plt.subplot(nrows, ncols, index + 1)
+        plt.imshow(data[index].reshape(height, width), cmap=cmap)
+        plt.title(str(trueLabel[indexesMisclassified[index]]) + " : " + str(predictedLabel[indexesMisclassified[index]]))
+        plt.axis('off')
+    plt.ioff()
+    fig.suptitle(title)
+    return plt
 
+def compareResults(train: any,
+                   val: any,
+                   title: str,
+                   xlabel: str,
+                   ylabel: str,
+                   trainColor: str='blue',
+                   valColor: str='green'):
+    plt.figure(figsize=(10, 5))
+    plt.plot(train, color=trainColor)
+    plt.plot(val, color=valColor)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.axis('off')
+    plt.ioff()
+    return plt
 
+def show_confusion_matrix(testing : any,
+                          predictions : any,
+                          num_classes : any,
+                          labels : any):
+    matrix = confusion_matrix(testing, predictions)
+    plt.figure(figsize=(10,5))
+    hm = sns.heatmap(matrix,
+                     cmap='coolwarm',
+                     linecolor='white',
+                     linewidths=1,
+                     xticklabels=labels,
+                     yticklabels=labels,
+                     annot=True,
+                     fmt='d')
+    plt.yticks(rotation = 0)  # Don't rotate (vertically) the y-axis labels
+    #hm.invert_yaxis() # Invert the labels of the y-axis
+    hm.set_ylim(0, len(matrix))
+    plt.title('Confusion Matrix')
+    plt.ylabel('True Label')
+    plt.xlabel('Predicted Label')
+    plt.ioff()
+    return plt
